8c26f028350b54ebe107e421cf2240c366d1a238
juliansalvador727 <juliansalvador727@gmail.com>
2025-05-15 22:06:19 -0700

    more commit

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-function createGrid(rows) {
-  const container = document.getElementById("container");
+const container = document.getElementById("container");
+const containerWidthPx = 640;
+const containerHeightPx = 640;
+
+let R = randomBetween(0, 255);
+let G = randomBetween(0, 255);
+let B = randomBetween(0, 255);
+
+function addHoverEffect(square) {
+  square.addEventListener("mouseover", () => {
+    square.style.backgroundColor = "black";
+  });
+}
+
+function createGrid(gridSquare) {
+  const rows = gridSquare * gridSquare;
 
   for (let i = 1; i <= rows; i++) {
     const square = document.createElement("div");
     square.classList.add("grid-square");
+    addHoverEffect(square);
+    square.style.width = `${containerWidthPx / gridSquare}px`;
+    square.style.height = `${containerHeightPx / gridSquare}px`;
     container.appendChild(square);
   }
 }
 
-createGrid(16);
+createGrid(4);
